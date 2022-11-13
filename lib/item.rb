e482f4dc8b06650cff347d4548457dc1508590e3
KaraJH <karas.jones@yahoo.com>
2022-11-13 15:16:05 -0700

    Creates passing test for current high bid

--- lib/item.rb
+++ lib/item.rb
@@ -9,4 +9,11 @@ class Item
   def add_bid(attendee_object, bid_num)
     @bids[attendee_object] = bid_num
   end
+
+  def current_high_bid
+    find_high = @bids.max_by do |attendee, bid|
+      bid
+    end
+    find_high.last
+  end
 end
