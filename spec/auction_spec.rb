8453444b3d692d0d90e4c483c45f4a4c4d886f1d
KaraJH <karas.jones@yahoo.com>
2022-11-13 14:38:54 -0700

    Creates tests for auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,1 +1,37 @@
+require './lib/auction'
+require './lib/item'
 
+RSpec.describe Auction do
+  it 'exists and has attributes' do
+    auction = Auction.new
+
+    expect(auction).to be_a(Auction)
+    expect(auction.items).to eq([])
+  end
+
+  describe '#add_item' do
+    xit 'can add items to the auction array' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.items).to eq([item1, item2])
+    end
+  end
+
+  describe '#item_names' do
+    xit 'can list the names of each item' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+  end
+end
