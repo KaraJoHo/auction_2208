ae35dc90c2c631b9a37cc1ce26c86a3971842771
KaraJH <karas.jones@yahoo.com>
2022-11-13 14:23:18 -0700

    Tests for item object and attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,16 +1,14 @@
-require 'spec_helper'
+require 'rspec'
+require './lib/item'
 
 RSpec.describe Item do
-  before (:each) do
-    @item1 = Item.new('Chalkware Piggy Bank')
-    @item2 = Item.new('Bamboo Picture Frame')
-  end
+  it 'exists and has attributes' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
 
-  describe '#initialize' do
-    it 'exsists and has readable attributes' do
-
-      expect(@item1).to be_an_instance_of(Item)
-      expect(@item1.name).to eq("Chalkware Piggy Bank")
-    end
+    expect(item1).to be_a(Item)
+    expect(item2).to be_a(Item)
+    expect(item1.name).to eq('Chalkware Piggy Bank')
+    expect(item2.name).to eq('Bamboo Picture Frame')
   end
 end
